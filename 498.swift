3b01aad379f46af41a32f75c2cff98bcd4fd7a64
preccrep <62749211+preccrep@users.noreply.github.com>
2022-06-14 18:58:07 +0800

    Create 498.swift

---
+++ 498.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func findDiagonalOrder(_ mat: [[Int]]) -> [Int] {
+        var ans: [Int] = []
+        let (m, n) = (mat.count, mat[0].count)
+        let maxv = m + n - 1
+        for i in 0..<maxv {
+            if i & 1 == 0 {
+                let lowBound = max(0, i - m + 1)
+                let upBound = min(n - 1, i)
+                for j in lowBound...upBound {
+                    ans.append(mat[i - j][j])
+                }
+            } else {
+                let lowBound = max(0, i - n + 1)
+                let upBound = min(m - 1, i)
+                for j in lowBound...upBound {
+                    ans.append(mat[j][i - j])
+                }
+            }
+        }
+        return ans
+    }
+}
